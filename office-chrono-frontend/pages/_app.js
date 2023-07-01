79fc0ef08f12fb4c1b6e7e5af12ad6540e4179d3
Kahnu Charan Swain <djkanha044@gmail.com>
2023-07-02 05:23:11 +0530

    isAuthenticated added

--- office-chrono-frontend/pages/_app.js
+++ office-chrono-frontend/pages/_app.js
@@ -1,7 +1,22 @@
 import '@/styles/globals.css'
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const isAuthenticated = !!localStorage.getItem('token'); // Check if the token is present in local storage
+
+    if (isAuthenticated) {
+      router.push('/dashboard');
+    } else {
+      router.push('/');
+    }
+  }, []);
+
   return (
     <>
       <Head>
